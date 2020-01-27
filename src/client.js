310c35325cb5b7184c33146e1517104dd29a7a83
carro311 <carro311@su17-107.ad.liu.se>
2020-01-27 12:02:04 +0100

    signup info created

--- src/client.js
+++ src/client.js
@@ -8,3 +8,24 @@ displayView=function(view){
 window.onload=function(){
   displayView(document.getElementById('welcomeview'));
 };
+
+signupValidation=function(form){
+  var firstName = form.firstName.value.trim();
+  var familyName = form.familyName.value.trim();
+  var gender = form.gender.value.trim();
+  var city = form.city.value.trim();
+  var country = form.country.value.trim();
+  var email = form.email.value.trim();
+  var password = form.password.value.trim();
+  var repPassword = form.repPassword.value.trim();
+
+
+  window.alert(firstName);
+
+  //var info = [];
+  //info = JSON.stringify(form);
+  //var obj = JSON.parse(info);
+};
+
+signinValidation=function(form){
+};
